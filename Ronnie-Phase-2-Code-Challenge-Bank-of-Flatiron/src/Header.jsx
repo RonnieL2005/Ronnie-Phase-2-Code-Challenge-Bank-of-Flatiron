1a9169cf00617880f2ffd0299eefbdb67697fa22
Ronnielangat <ronnielangat2020@gmail.com>
2024-02-12 07:47:49 +0300

    mm

---
+++ Ronnie-Phase-2-Code-Challenge-Bank-of-Flatiron/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//this function is used to show the name of the bank
+
+function Header(){
+    return(
+    <div>
+    <header className="App-header">
+     <h1>Flatiron Bank</h1>
+    </header>
+    </div>
+    )
+}
+export default Header;
